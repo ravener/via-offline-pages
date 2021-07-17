64dc420dc15271c54a4ad39d0909a169f2d989fc
Raven Ravener <ravener.anime@gmail.com>
2021-07-17 08:00:27 +0430

    add jump to top button

--- index.js
+++ index.js
@@ -73,9 +73,47 @@ let html = `
       font-weight: 400;
       line-height: 1.25;
     }
+
+    #myBtn {
+      display: none; /* Hidden by default */
+      position: fixed; /* Fixed/sticky position */
+      bottom: 20px; /* Place the button at the bottom of the page */
+      right: 30px; /* Place the button 30px from the right */
+      z-index: 99; /* Make sure it does not overlap */
+      border: none; /* Remove borders */
+      outline: none; /* Remove outline */
+      background-color: red; /* Set a background color */
+      color: white; /* Text color */
+      cursor: pointer; /* Add a mouse pointer on hover */
+      padding: 15px; /* Some padding */
+      border-radius: 10px; /* Rounded corners */
+      font-size: 18px; /* Increase font size */
+    }
+
+    #myBtn:hover {
+      background-color: #555; /* Add a dark-grey background on hover */
+    }
     </style>
   </head>
   <body>
+    <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+    <script>
+      const mybutton = document.getElementById("myBtn");
+      window.onscroll = function() {scrollFunction()};
+
+      function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          mybutton.style.display = "block";
+        } else {
+          mybutton.style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
     <p><strong>${pages.length}</strong> Pages.</p>
     <p>Total Size: <strong>${psize}</strong></p>
     <p class="subtitle">Generated at ${new Date().toDateString()}</p>\n`;
